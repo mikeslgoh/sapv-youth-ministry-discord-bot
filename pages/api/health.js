7aafbea28df987f5b33e78420345ebdc9fe5c5ff
Michael Goh <mikegoh1997@gmail.com>
2025-02-24 14:16:04 -0800

    add new endpoint:

--- pages/api/health.js
+++ pages/api/health.js
@@ -1,10 +1,3 @@
 export default function handler(req, res) {
-    if (req.method === 'GET') {
-        console.log(`[${new Date().toISOString()}] Received a ping from UptimeRobot!`);
-        res.setHeader('Content-Type', 'text/plain');
-        res.status(200).send('Bot is running smoothly!');
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).json({ message: `Method ${req.method} not allowed` });
-    }
+    res.status(200).json({ status: 'ok' });
 }
